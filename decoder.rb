c0bcd191fb890f880e1858adad37de8a6f7c17b8
Amine Smahi <md.amine.smahi@gmail.com>
2021-11-04 11:35:40 +0100

    Change the algorithm of decode_char method

--- decoder.rb
+++ decoder.rb
@@ -1,69 +1,22 @@
-alphabets = {"A" => ".-", "B" => ".---", "C" => "-.-.", "D" => "-..", "E" => ".", "F" => "--.", "G" => "--.","H" => "....", "I" => "..", "J" => ".---", "K" => "-.-", "L" => ".-..","M" => "--", "N" => "-.", "O" => "---", "P" => ".--.", "Q" => "--.-", "R" => ".-.", "S" => "...", "T" => "-", "U" => "..-", "V" => "...-", "W" => ".--", "X" => "-..-", "Y" => "-.--", "Z" => "--..",}
-
-
-def decode_char(letter)
-  case letter
-  when '.-'
-    'A'
-  when '-...'
-    'B'
-  when '-.-.'
-    'C'
-  when '-..'
-    'D'
-  when '.'
-    'E'
-  when '..-.'
-    'F'
-  when '--.'
-    'G'
-  when '....'
-    'H'
-  when '..'
-    'I'
-  when '.---'
-    'G'
-  when '-.-'
-    'K'
-  when '.-..'
-    'L'
-  when '--'
-    'M'
-  when '-.'
-    'N'
-  when '---'
-    'O'
-  when '.--.'
-    'P'
-  when '--.-'
-    'Q'
-  when '.-.'
-    'R'
-  when '...'
-    'S'
-  when '-'
-    'T'
-  when '..-'
-    'U'
-  when '...-'
-    'V'
-  when '.--'
-    'W'
-  when '-..-'
-    'X'
-  when '-.--'
-    'Y'
-  when '--..'
-    'Z'
-  else
-    '-'
+class Decoder
+  @alphabets = {
+    'A' => '.-', 'B' => '-...', 'C' => '-.-.', 'D' => '-..', 'E' => '.', 'F' => '..-.', 'G' => '--.',
+    'H' => '....', 'I' => '..', 'J' => '.---', 'K' => '-.-', 'L' => '.-..', 'M' => '--', 'N' => '-.', 'O' => '---',
+    'P' => '.--.', 'Q' => '--.-', 'R' => '.-.', 'S' => '...', 'T' => '-', 'U' => '..-', 'V' => '...-', 'W' => '.--',
+    'X' => '-..-', 'Y' => '-.--', 'Z' => '--..'
+  }
+  def self.decode_char(letter)
+    @alphabets.each do |key, value|
+      @a = key if value == letter
+    end
+    @a
   end
 end
 
 def decode_word(word)
   characters = word.split
   decoded_word = ''
-  characters.each { |char| decoded_word += (decode_char char) }
+  characters.each { |char| decoded_word += (Decoder.decode_char char) }
   decoded_word
 end
 
